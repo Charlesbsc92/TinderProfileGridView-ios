a302c356d203c76ee202a89340d8546cd8f3daed
Charles <charles@Charless-Mac-mini.local>
2023-12-10 17:42:06 +0530

    Instagram based profile completed by charles.

---
+++ TinderProfileView/TinderProfileView/ProfileHeaderView.swift
@@ -0,0 +1,17 @@
+//
+//  ProfileHeaderView.swift
+//  TinderProfileView
+//
+//  Created by Charles on 10/12/23.
+//
+
+import UIKit
+
+class ProfileHeaderView: UICollectionReusableView {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+}
--- TinderProfileView/TinderProfileView/ViewController.swift
+++ TinderProfileView/TinderProfileView/ViewController.swift
@@ -7,13 +7,45 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout {
+    
+    @IBOutlet weak var profileCV:UICollectionView!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.configUI()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    private func configUI() {
+        self.profileCV.delegate = self
+        self.profileCV.dataSource = self
+        registerViews()
+    }
+    
+    private func registerViews() {
+        self.profileCV.register(UINib(nibName: "ProfileHeaderView", bundle: nil), forSupplementaryViewOfKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ProfileHeaderView")
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = UICollectionViewCell()
+        return cell
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        let view = collectionView.dequeueReusableSupplementaryView(ofKind: UICollectionView.elementKindSectionHeader, withReuseIdentifier: "ProfileHeaderView", for: indexPath) as! ProfileHeaderView
+        return view
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        return CGSize(width: collectionView.frame.width, height: view.frame.height)
+    }
+    
+    
 }
 
